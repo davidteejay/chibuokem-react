7083dce42e7605b96698a72a2441b99d6b35d134
davidteejay <chibuokem_tolu@hotmail.com>
2017-12-08 16:10:32 +0100

    edit

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,13 +2,8 @@ import React, { Component } from 'react';
 
 export default class Footer extends Component {
     render(){
-        let style = {
-            display: 'inline'
-        }
-
         return (
             <div className="footer">
-            <div class="addthis_inline_follow_toolbox" style={style}></div>
             <p className="right-align light">&copy; Chibuokem Onyekwelu. All rights reserved</p>
         </div>
         )
