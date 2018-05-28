49362ba8c90b701a8b0ab67614d23690d256e05d
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2018-05-28 15:06:02 +0100

    Added more to the portfolio

--- src/components/wrapper3.js
+++ src/components/wrapper3.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+const url = "https://cdn.rawgit.com/davidteejay/chibuokem/master/src/";
 
 export default class Wrapper3 extends Component {
     render(){
@@ -8,34 +9,72 @@ export default class Wrapper3 extends Component {
                 <div className="underline2 slideanim"></div>
                 <div className="row" id="portfolio">
                     <div className="col s12 m6 center">
+                        <img src="https://cdn.rawgit.com/davidteejay/chibuokem/master/src/images/cpl.png" alt="Christ Praise Chapel Website"/>
+                        <a className="content" href="//thecplchapel.org.ng" target="_blank" rel="noopener noreferrer">
+                            <div style={{ width: '100%' }}>
+                                <h5 className="light center">
+                                    CHRIST PRAISE CHAPEL'S OFFICIAL WEBSITE
+                                </h5>
+                                <a href="//thecplchapel.org.ng" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="col s12 m6 center">
+                        <img src="https://cdn.rawgit.com/davidteejay/chibuokem/master/src/images/incycle.png" alt="Incycle"/>
+                        <a className="content" href="//incycle.com.ng" target="_blank" rel="noopener noreferrer">
+                            <div style={{ width: '100%' }}>
+                                <h5 className="light center">
+                                    INCYCLE'S OFFICIAL LANDING PAGE
+                                </h5>
+                                <a href="//incycle.com.ng" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="col s12 m6 center">
                         <img src="https://cdn.rawgit.com/davidteejay/chibuokem/master/src/images/acm.png" alt="FUTA ACM's Website"/>
-                        <h5 className="light center">
-                            ASSOCIATION FOR COMPUTING AND MACHINERY: FUTA CHAPTER
-                        </h5>
-                        <a href="//futa.acm.org" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                        <a className="content" href="//futa.acm.org" target="_blank" rel="noopener noreferrer">
+                            <div style={{ width: '100%' }}>
+                                <h5 className="light center">
+                                    ASSOCIATION FOR COMPUTING AND MACHINERY: FUTA CHAPTER
+                                </h5>
+                                <a href="//futa.acm.org" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                            </div>
+                        </a>
                     </div>
                     <div className="col s12 m6 center">
                         <img src="https://cdn.rawgit.com/davidteejay/chibuokem/master/src/images/alumni.png" alt="FUTA Alumni's Website"/>
-                        <h5 className="light center">
-                            FUTA ALUMNI ASSOCIATION WORLDWIDE 
-                        </h5>
-                        <a href="//futaalumniworldwide.org" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                        <a className="content" href="//futaalumniworldwide.org" target="_blank" rel="noopener noreferrer">
+                            <div style={{ width: '100%' }}>
+                                <h5 className="light center">
+                                    FUTA ALUMNI ASSOCIATION WORLDWIDE 
+                                </h5>
+                                <a href="//futaalumniworldwide.org" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                            </div>
+                        </a>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col s12 m6 center">
                         <img src="https://cdn.rawgit.com/davidteejay/chibuokem/master/src/images/bsb.png" alt="Best Solution Book's Website"/>
-                        <h5 className="light center">
-                            BEST SOLUTION BOOKS
-                        </h5>
-                        <a href="//bestsolutionbooks.com" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                        <a className="content" href="//bestsolutionbooks.com" target="_blank" rel="noopener noreferrer">
+                            <div style={{ width: '100%' }}>
+                                <h5 className="light center">
+                                    BEST SOLUTION BOOKS
+                                </h5>
+                                <a href="//bestsolutionbooks.com" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                            </div>
+                        </a>
                     </div>
                     <div className="col s12 m6 center">
                         <img src="https://cdn.rawgit.com/davidteejay/chibuokem/master/src/images/alawhyte.png" alt="Alawhyte's Website"/>
-                        <h5 className="light center">
-                            ALAWHYTE
-                        </h5>
-                        <a href="//alawhyte.com" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                        <a className="content" href="//alawhyte.com" target="_blank" rel="noopener noreferrer">
+                            <div style={{ width: '100%' }}>
+                                <h5 className="light center">
+                                    ALAWHYTE
+                                </h5>
+                                <a href="//alawhyte.com" target="_blank" rel="noopener noreferrer">VIEW PROJECT</a>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
